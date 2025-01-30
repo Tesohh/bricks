19383714131facb23098af8a7b4710fa15a87fb8
Simone Tesini <simozilla006@gmail.com>
2025-01-30 18:18:01 +0100

    add blueprints definition

--- src/cli/install/mod.rs
+++ src/cli/install/mod.rs
@@ -1,10 +1,17 @@
+use std::{collections::HashMap, fs};
+
 use anyhow::Result;
 
-use crate::{config::Config, libs::install_lib::install_lib};
+use crate::{
+    config::Config,
+    libs::{blueprint::BlueprintFile, install_lib::install_lib},
+};
 
 pub fn install(config: Config) -> Result<()> {
+    let mut blueprints = BlueprintFile { bp: HashMap::new() };
+
     for (name, lib) in &config.libs {
-        install_lib(name, lib)?;
+        install_lib(name, lib, &mut blueprints)?;
     }
 
     Ok(())
--- src/libs/install_lib.rs
+++ src/libs/install_lib.rs
@@ -1,42 +1,54 @@
-use std::{
-    fs::{self},
-    io::Write,
-    path::Path,
-};
-
 use anyhow::Result;
 
-use crate::{
-    cli::{init::templates, pretty},
-    config::lib::{Lib, LibKind},
-};
+use crate::config::lib::{Lib, LibKind};
 
-pub fn install_lib(name: &str, lib: &Lib) -> Result<()> {
+use super::blueprint::{Blueprint, BlueprintFile};
+
+pub fn install_lib(name: &str, lib: &Lib, blueprints: &mut BlueprintFile) -> Result<()> {
     match lib.kind {
         LibKind::System => {
-            let clangd_path = Path::new(".clangd");
-            pretty::msg(
-                "add to .clangd",
-                format!("{} version {}", name, lib.version),
-            );
-
-            match fs::exists(clangd_path) {
-                Ok(false) => {
-                    fs::write(clangd_path, templates::clangd())?;
-                }
-                Err(_) => {
-                    fs::write(clangd_path, templates::clangd())?;
-                }
-                _ => {}
-            }
-
-            let mut append_file = fs::OpenOptions::new().append(true).open(clangd_path)?;
-
-            let new_entry = format!("    - \"{}\"\n", lib.headers(name)?);
-            append_file.write_all(new_entry.as_bytes())?;
-
-            Ok(())
+            // you don't need to do anything here.
         }
-        LibKind::Git => todo!(),
-    }
+        LibKind::Git => {
+            // if the library isn't already installed:
+            // git clone it from the provided source
+            // in the library's directory:
+            // run bricks install
+            // run bricks build
+            //
+            // if it's already installed, you don't need to do anything.
+        }
+    };
+
+    // Get lib and header directory from Lib::lib_links and Lib::header
+    let libs = lib.lib_links(name)?;
+    let headers = lib.headers(name)?;
+
+    // Add blueprint entry to blueprintfile
+    blueprints
+        .bp
+        .insert(name.to_string(), Blueprint { libs, headers });
+
+    // Add to clangd
+    // let clangd_path = Path::new(".clangd");
+    // pretty::msg(
+    //     "add to .clangd",
+    //     format!("{} version {}", name, lib.version),
+    // );
+
+    // match fs::exists(clangd_path) {
+    //     Ok(false) => {
+    //         fs::write(clangd_path, templates::clangd())?;
+    //     }
+    //     Err(_) => {
+    //         fs::write(clangd_path, templates::clangd())?;
+    //     }
+    //     _ => {}
+    // }
+    //
+    // let mut append_file = fs::OpenOptions::new().append(true).open(clangd_path)?;
+    //
+    // let new_entry = format!("    - \"{}\"\n", lib.headers(name)?);
+    // append_file.write_all(new_entry.as_bytes())?;
+    Ok(())
 }
